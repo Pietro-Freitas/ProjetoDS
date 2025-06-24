c7f819b5c12bc7c715655eeb37b95a6e20b6a91f
Pietro-Freitas <pietrocardosof@gmail.com>
2025-06-24 11:12:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,1 @@
-import streamlit as st
-from funcoes import cadastrar
-from funcoes import visualizar
-from funcoes import editar
 
-with st.sidebar:
-    st.title("Menu Lateral")
-    opcao = st.selectbox("Opções:", ["Cadastrar Tarefas", "Visualizar Tarefas", "Editar Tarefas"])
-
-if opcao == "Cadastrar Tarefas":
-    cadastrar()
-elif opcao == "Visualizar Tarefas":
-    visualizar()
-elif opcao == "Editar Tarefas":
-    editar()
